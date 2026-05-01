45f6db4ea6dadd6fe67dcded9aaa4789dbb425d9
Robert Pannick <rwpannick@gmail.com>
2026-04-30 23:42:00 -0400

    initial commit
    Signed-off-by: Robert Pannick <rwpannick@gmail.com>

---
+++ src/deep_research_engine/crew.py
@@ -0,0 +1,226 @@
+import os
+
+from crewai import LLM
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import (
+	SerperDevTool,
+	EXASearchTool,
+	ScrapeWebsiteTool,
+	ArxivPaperTool
+)
+
+
+
+
+
+
+@CrewBase
+class DeepResearchEngineCrew:
+    """DeepResearchEngine crew"""
+
+    
+    @agent
+    def senior_research_strategist(self) -> Agent:
+        
+        
+        return Agent(
+            config=self.agents_config["senior_research_strategist"],
+            
+            
+            tools=[				SerperDevTool(),
+				EXASearchTool(),
+				ScrapeWebsiteTool()],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            
+            
+            max_execution_time=None,
+            llm=LLM(
+                model="groq/llama-3.3-70b-versatile",
+                temperature=0.7,
+                
+            ),
+            
+        )
+        
+    
+    @agent
+    def other_steve_deep_research_mode(self) -> Agent:
+        
+        
+        return Agent(
+            config=self.agents_config["other_steve_deep_research_mode"],
+            
+            
+            tools=[				SerperDevTool(),
+				EXASearchTool(),
+				ScrapeWebsiteTool(),
+				ArxivPaperTool()],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            
+            
+            max_execution_time=None,
+            llm=LLM(
+                model="openai/openai/gpt-4o-mini",
+                
+                
+            ),
+            
+        )
+        
+    
+    @agent
+    def other_steve_sift_fact_checker(self) -> Agent:
+        
+        
+        return Agent(
+            config=self.agents_config["other_steve_sift_fact_checker"],
+            
+            
+            tools=[				SerperDevTool(),
+				EXASearchTool(),
+				ScrapeWebsiteTool()],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            
+            
+            max_execution_time=None,
+            llm=LLM(
+                model="openai/openai/gpt-4o-mini",
+                
+                
+            ),
+            
+        )
+        
+    
+    @agent
+    def other_steve_pragmatic_editor(self) -> Agent:
+        
+        
+        return Agent(
+            config=self.agents_config["other_steve_pragmatic_editor"],
+            
+            
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            
+            
+            max_execution_time=None,
+            llm=LLM(
+                model="openai/mistralai/mistral-large",
+                
+                
+            ),
+            
+        )
+        
+    
+    @agent
+    def other_steve_tree_of_thoughts_evaluator(self) -> Agent:
+        
+        
+        return Agent(
+            config=self.agents_config["other_steve_tree_of_thoughts_evaluator"],
+            
+            
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            
+            
+            max_execution_time=None,
+            llm=LLM(
+                model="openai/minimax/minimax-m1",
+                
+                
+            ),
+            
+        )
+        
+    
+
+    
+    @task
+    def strategy_formulation(self) -> Task:
+        return Task(
+            config=self.tasks_config["strategy_formulation"],
+            markdown=False,
+            
+            
+        )
+    
+    @task
+    def deep_technical_research_execution(self) -> Task:
+        return Task(
+            config=self.tasks_config["deep_technical_research_execution"],
+            markdown=False,
+            
+            
+        )
+    
+    @task
+    def sift_fact_checking_and_verification(self) -> Task:
+        return Task(
+            config=self.tasks_config["sift_fact_checking_and_verification"],
+            markdown=False,
+            
+            
+        )
+    
+    @task
+    def research_methodology_evaluation(self) -> Task:
+        return Task(
+            config=self.tasks_config["research_methodology_evaluation"],
+            markdown=False,
+            
+            
+        )
+    
+    @task
+    def technical_content_synthesis(self) -> Task:
+        return Task(
+            config=self.tasks_config["technical_content_synthesis"],
+            markdown=False,
+            
+            
+        )
+    
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the DeepResearchEngine crew"""
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+
+            chat_llm=LLM(model="openai/google/gemini-2.5-flash"),
+        )
+
+
